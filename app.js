601f86f2234e743c61c853813916b0d32c5429db
MZsteezy <zm77123@gmail.com>
2024-09-14 23:07:11 +0100

    inital comment

---
+++ app.js
@@ -0,0 +1,18 @@
+// Scroll to top button functionality
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    let scrollButton = document.getElementById("scrollTopBtn");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        scrollButton.style.display = "block";
+    } else {
+        scrollButton.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
